0022629ebd9602f7697db238f59725eed9d403e4
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-10-29 10:14:09 +0300

    creating the seed file for the companies

---
+++ db/seeds/companies.rb
@@ -0,0 +1,22 @@
+Company.create!([
+  { name: "3sixD", description: "3SIXD CONSULTING",
+    sub_description: "We create openings - Potential candidate apply for the job - We will hire the 
+    succesfully candidate", 
+    about: "3sixD is the site where recruiter posts various jobs to proffesionals/contractors online 
+    and them depending on the job/service they are qualified for will apply for that job. Finally a 
+    recruiter will be able to select the qualified pessonel depending on his needs for the job he 
+    posted.", 
+    location: '3sixD Corporation, USA-78995', facebook: "https://facebook.com", 
+    twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
+    linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com"
+  },
+  { name: "Furaha", description: "Furaha Software Inc,",
+    sub_description: "The center for Software Development - Hire us - We will provide you with the 
+    best software in turn", 
+    about: "Furaha Software Inc is one of the best software development company in the planet, 
+    For more visit at <a href='http://www.furahasoftware.com/'>Furaha Web</a>", 
+    location: 'Furaha Software Inc, USA-78995', facebook: "https://facebook.com", 
+    twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
+    linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com"
+  }
+])
