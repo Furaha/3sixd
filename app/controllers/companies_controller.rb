4498b9d8688f432d48b45d3e3aa89cb8a9a7049f
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-11-02 10:21:46 +0300

    updating the controllers to handle the company update of its details

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,29 @@
 class CompaniesController < ApplicationController
   def new
+  	@company = Company.new
+  end
+
+  def edit
+    @company =  Company.find(params[:id])
+  end
+
+  def update
+  	@company =  Company.find(params[:id])
+  	respond_to do |format|
+  	  if @company.update(company_params)
+  	    flash[:success] = "Successfully updated the view contents for company: #{@company.name}!"
+  	    format.html { redirect_to root_path }
+  	  else
+  	    flash[:danger] = "Sorry, unable to update the view contents for the company: #{@company.name}"
+  	    format.html { render action: edit }
+  	  end
+  	end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :sub_description, :about, :address, :facebook, 
+    	:google_plus, :twitter, :linkedin, :dribble)
   end
 end
