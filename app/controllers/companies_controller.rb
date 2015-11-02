cd69bd6248ad4cdd98fa97f7748a7c8d21a16442
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-11-02 12:02:49 +0300

    changing the location for displaying the flash messages

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -11,7 +11,7 @@ class CompaniesController < ApplicationController
     @company =  Company.find(params[:id])
     respond_to do |format| 
       if @company.update(company_params)
-        flash[:success] = "Successfully updated the view contents for company: #{@company.name}!"
+        flash[:success] = "Successfully updated the view contents for company: #{@company.name}"
   	    format.html { redirect_to root_path }
       else
         flash[:danger] = "Sorry, unable to update the view contents for the company: #{@company.name}"
