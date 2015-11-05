2abd451e7cb08ff0f3867c473d030e26525e5389
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-11-05 13:28:23 +0300

    adding the tests for other actions in companies controller

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class CompaniesControllerTest < ActionController::TestCase
-  test "should get new" do
+  def setup
+    @company = companies(:Furaha)
+  end
+
+  test "should get add new company page" do
     get :new
     assert_response :success
   end
 
+  test "should get edit existing company page" do
+    get :edit, id: @company.id
+    assert_response :success
+  end
+
+  test "should get all companies view page" do
+    get :index
+    assert_response :success
+  end
 end
