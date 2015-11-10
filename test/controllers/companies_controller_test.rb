e858323a6a4f75c21e8a9b6223b75f69ea7480ff
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-11-10 11:31:39 +0300

    adding the tests for the changes made on uploding an image to the app

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -5,6 +5,30 @@ class CompaniesControllerTest < ActionController::TestCase
     @company = companies(:Furaha)
   end
 
+  def file
+    @file ||= File.open(File.expand_path( '../../file/3sixd.png', __FILE__))
+  end
+
+  def uploaded_file_object(klass, attribute, file, content_type = 'image/png')
+    filename = File.basename(file.path)
+    klass_label = klass.to_s.underscore
+   
+    ActionDispatch::Http::UploadedFile.new(
+      tempfile: file,
+      filename: filename,
+      head: %Q{Content-Disposition: form-data; name="#{klass_label}[#{attribute}]"; filename="#{filename}"},
+      content_type: content_type
+    )
+  end
+
+  def test_logo_uploading
+    name = '3sixD'
+    assert_response 200
+    company = Company.last
+    assert_equal name, company.name
+    assert_equal(File.basename(file.path), company.logo_identifier)
+  end
+
   test "should get add new company page" do
     get :new
     assert_response :success
