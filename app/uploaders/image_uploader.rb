54e4ca5295a644d9a69d7c6be5da4b99a5ada88a
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-11-10 18:26:24 +0300

    nice logo dimensions customization

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [350, 120]
+    process :resize_to_fill => [700, 133]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
