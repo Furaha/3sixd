41d81c108caae51b4fafbcbbbbf0afeb697eca32
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-12-03 12:43:46 +0300

    removing the line which is already in tests_helper and indent the file well

--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
-
 class JobsControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
   test "should get index" do
-  	get :index
-  	assert_response :success
+    get :index
+    assert_response :success
   end
 end
