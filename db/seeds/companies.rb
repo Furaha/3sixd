862c43abfe92c74dfba8b5a89534b4bec82bc649
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-12-10 09:00:02 +0300

    updating the seed file to reflect the address changes

--- db/seeds/companies.rb
+++ db/seeds/companies.rb
@@ -6,8 +6,8 @@ Company.create!([
     about: "3sixD is the site where recruiter posts various jobs to proffesionals/contractors online 
     and them depending on the job/service they are qualified for will apply for that job. Finally a 
     recruiter will be able to select the qualified pessonel depending on his needs for the job he 
-    posted.", 
-    address: 'USA-78995', facebook: "https://facebook.com", 
+    posted.", address1: "700 Craighead St", address2: "Suite 106", city: "Nashville", state: "TN",
+    zip: "37204", facebook: "https://facebook.com", 
     twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
     linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com",
     default: 'true'
@@ -18,7 +18,8 @@ Company.create!([
     best software in turn", 
     about: "Furaha Software Inc is one of the best software development company in the planet, 
     For more visit at <a href='http://www.furahasoftware.com/'>Furaha Web</a>", 
-    address: 'USA-78995', facebook: "https://facebook.com", 
+    address1: "701 Craighead St", address2: "Suite 107", city: "Nashville", state: "TN",
+    zip: "37207", facebook: "https://facebook.com", 
     twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
     linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com",
     default: 'false'
