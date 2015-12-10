0d5581b49083635213dca4589f9b29bf69d0e0a5
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-12-10 10:57:06 +0300

    validating the lenght for zip code and state

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,13 +2,15 @@ class Company < ActiveRecord::Base
   mount_uploader :logo,        ImageUploader
   mount_uploader :intro_image, IntroImageUploader
   
-  validates :name,            presence: true
-  validates :title,           presence: true
-  validates :description,     presence: true
-  validates :about,           presence: true
-  validates :address1,        presence: true
-  validates :city,            presence: true
-  validates :state,           presence: true
-  validates :zip,             presence: true
+  validates :name,        presence: true
+  validates :title,       presence: true
+  validates :description, presence: true
+  validates :about,       presence: true
+  validates :address1,    presence: true
+  validates :city,        presence: true
+  validates :state,       presence: true
+  validates :zip,         presence: true, length: { is: 5 }
+  validates :state,       presence: true, length: { is: 2 }, format: { with: /\A[A-Z]+\z/,
+    message: "only allows 2 capital letters" }
   has_many  :users
 end
