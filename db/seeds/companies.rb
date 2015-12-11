d94a4621e7dc80929594a82049b4203cf8f45455
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-12-11 09:38:07 +0300

    updating the companies seed file to have State.find_by_code('State Name').id with .id at end so as to create the states ids in the companies table

--- db/seeds/companies.rb
+++ db/seeds/companies.rb
@@ -7,7 +7,7 @@ Company.create!([
     and them depending on the job/service they are qualified for will apply for that job. Finally a 
     recruiter will be able to select the qualified pessonel depending on his needs for the job he 
     posted.", address1: "700 Craighead St", address2: "Suite 106", city: "Nashville", 
-    state_id: State.find_by_code('TN'), zip: "37204", facebook: "https://facebook.com", 
+    state_id: State.find_by_code('TN').id, zip: "37204", facebook: "https://facebook.com", 
     twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
     linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com", default: 'true'
   },
@@ -18,7 +18,7 @@ Company.create!([
     about: "Furaha Software Inc is one of the best software development company in the planet, 
     For more visit at <a href='http://www.furahasoftware.com/'>Furaha Web</a>", 
     address1: "701 Craighead St", address2: "Suite 107", city: "Nashville", 
-    state_id: State.find_by_code('IL'), zip: "37207", facebook: "https://facebook.com", 
+    state_id: State.find_by_code('IL').id, zip: "37207", facebook: "https://facebook.com", 
     twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
     linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com", default: 'false'
   }
