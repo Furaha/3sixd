2f1a83c65badefd1d2b1a7539d1b96faab62b2e7
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-12-11 09:22:57 +0300

    updating the companies seed file since state has changed to state_id

--- db/seeds/companies.rb
+++ db/seeds/companies.rb
@@ -6,11 +6,10 @@ Company.create!([
     about: "3sixD is the site where recruiter posts various jobs to proffesionals/contractors online 
     and them depending on the job/service they are qualified for will apply for that job. Finally a 
     recruiter will be able to select the qualified pessonel depending on his needs for the job he 
-    posted.", address1: "700 Craighead St", address2: "Suite 106", city: "Nashville", state: "TN",
-    zip: "37204", facebook: "https://facebook.com", 
+    posted.", address1: "700 Craighead St", address2: "Suite 106", city: "Nashville", 
+    state_id: State.find_by_code('TN'), zip: "37204", facebook: "https://facebook.com", 
     twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
-    linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com",
-    default: 'true'
+    linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com", default: 'true'
   },
   { 
     name: "Furaha", title: "Furaha Software Inc,",
@@ -18,10 +17,9 @@ Company.create!([
     best software in turn", 
     about: "Furaha Software Inc is one of the best software development company in the planet, 
     For more visit at <a href='http://www.furahasoftware.com/'>Furaha Web</a>", 
-    address1: "701 Craighead St", address2: "Suite 107", city: "Nashville", state: "TN",
-    zip: "37207", facebook: "https://facebook.com", 
+    address1: "701 Craighead St", address2: "Suite 107", city: "Nashville", 
+    state_id: State.find_by_code('IL'), zip: "37207", facebook: "https://facebook.com", 
     twitter: "https://twitter.com", google_plus: "https://plus.google.com", 
-    linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com",
-    default: 'false'
+    linkedin: "https://www.linkedin.com", dribble: "https://dribbble.com", default: 'false'
   }
 ])
