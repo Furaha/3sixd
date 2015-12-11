b391a2b4f0f0a7673edbc4ce94e4343aef0de732
Godfrey Mlowe <mlowegodfrey@gmail.com>
2015-12-11 08:36:05 +0300

    $ bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210045741) do
+ActiveRecord::Schema.define(version: 20151211053231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20151210045741) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string   "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
